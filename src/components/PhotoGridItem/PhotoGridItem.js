f34fe6906e8e2d57a1898dcf1ed51f05cbeb1e05
Ross Larner <ross.larner@quizlet.com>
2023-09-13 08:32:38 -0700

    attempt at ellipsis

--- src/components/PhotoGridItem/PhotoGridItem.js
+++ src/components/PhotoGridItem/PhotoGridItem.js
@@ -55,6 +55,8 @@ const Tag = styled.li`
   font-size: 0.875rem;
   font-weight: 475;
   color: var(--color-gray-800);
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
 
 export default PhotoGridItem;
